a0aed9079cb49fcab0755f394f899554ef176b51
Tom Bee <tburzynski2@yahoo.com>
2024-06-30 18:48:11 -0500

    modularized db connection, starter file for Inquirer prompts

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,32 +1,17 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-// Database connection configuration
 const client = new Client({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
 });
 
-// Connect to the database
 client
   .connect()
-  .then(() => {
-    console.log("Connected to the database");
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.error("Connection error", err.stack));
 
-    // Test query
-    // const query = "SELECT * FROM employees";
-    // return client.query(query);
-  })
-  .then((res) => {
-    console.log("Query results:", res.rows);
-  })
-  .catch((err) => {
-    console.error("Error executing query:", err.stack);
-  })
-  .finally(() => {
-    // Close the database connection
-    client.end();
-  });
+module.exports = client;
